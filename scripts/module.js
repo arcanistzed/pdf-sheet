2127dfd0d27b9489c45ea07276a64fa717547871
arcanist <arcanistzed@gmail.com>
2021-08-18 10:26:12 -0400

    feat: uses character sheet values

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,15 @@
+// Store mapping
+Hooks.on("init", () => {
+	game.settings.register(Pdfconfig.ID, "map", {
+		name: game.i18n.localize("pdfsheet.settings.map.Name"),
+		hint: game.i18n.localize("pdfsheet.settings.map.Hint"),
+		scope: "world",
+		config: true,
+		type: String,
+	});
+});
+
+// Add button to Actor Sheet for opening app
 Hooks.on("getActorSheetHeaderButtons", (sheet, buttons) => {
 	if (sheet.actor.type !== "character") return;
 
@@ -39,6 +51,7 @@ class Pdfconfig extends FormApplication {
 		return context;
 	};
 
+	/** @override */
 	activateListeners(html) {
 		document.getElementById("pdf-upload").addEventListener("change", event => {
 			var file = event.target.files[0];
@@ -49,11 +62,12 @@ class Pdfconfig extends FormApplication {
 
 		document.getElementById("pdf-download").addEventListener("click", event => {
 			event.preventDefault();
-			this.fillFields(currentBuffer);
+			this.download(currentBuffer);
 		});
 	};
 
-	listInputs(buffer) {
+	/** Create a form with inputs for each PDF field and fill them in with Actor data */
+	createForm(buffer) {
 		const inputForm = document.getElementById("fieldList");
 
 		// Empty input form
@@ -62,31 +76,41 @@ class Pdfconfig extends FormApplication {
 			inputForm.removeChild(child);
 		};
 
+		// Get PDF fields
 		const pdfFields = pdfform(minipdf).list_fields(buffer);
 
-		const rawMap = [
-			{ "pdf": "CharacterName", "foundry": "@name", "type": "string" },
-			{ "pdf": "Check Box 11", "foundry": "@data.abilities.str.proficient", "type": "string" },
-		];
+		// Get mapping
+		const rawMap = game.settings.get(Pdfconfig.ID, "map");
 
+		// Exit if map is invalid
+		if (!rawMap?.length) { ui.notifications.error("PDF Sheet | Invalid Map JSON"); return };
+
+		// Get Actor Data
 		const actorData = this.actor.data;
-		console.log(actorData)
+		console.log("PDF Sheet | Actor Data:", actorData)
 
-		rawMap.forEach(entry => {
+		// Parse values correctly
+		rawMap.forEach((entry, i) => {
 			// Does this entry contain a dynamic key
 			if (entry.foundry.includes("@")) {
-				let paramArray = entry.foundry.slice(1).split(".");
+				rawMap[i].foundry = getProperty(actorData, entry.foundry.slice(1));
+			}
 
-				let current = actorData;
-				paramArray.forEach(param => current += `['${param}']`)
+			// Parse Booleans
+			else if (entry.foundry === "true") { rawMap[i].foundry = true }
+			else if (entry.foundry === "false") { rawMap[i].foundry = false }
 
-				console.log(current);
-			} else {
+			// Parse Numbers (floats)
+			else if (parseFloat(entry.foundry) - parseFloat(entry.foundry) === 0) { rawMap[i].foundry = parseFloat(entry.foundry) }
 
+			// Parse regular strings
+			else {
+				rawMap[i].foundry = entry.foundry;
 			};
+			console.log(rawMap[i].foundry);
 		});
 
-		console.log("pdf fields:", pdfFields);
+		console.log("PDF Sheet | PDF fields:", pdfFields);
 
 		for (let pdfFieldKey in pdfFields) {
 
@@ -121,14 +145,11 @@ class Pdfconfig extends FormApplication {
 				input.setAttribute("data-idx", i);
 				input.setAttribute("data-key", pdfFieldKey);
 
+				// Make a checkbox if the type is boolean
 				if (field.type === "boolean") {
-
 					input.setAttribute("type", "checkbox");
 
-				} else if (field.type === "string") {
-
-					//input.setAttribute('value',);
-
+					// Make a drop down menu if the type is seelect and it has options
 				} else if ((field.type === "select") && field.options) {
 
 					field.options.forEach((value) => {
@@ -139,12 +160,24 @@ class Pdfconfig extends FormApplication {
 						input.appendChild(option);
 					});
 				};
-				row.appendChild(input);
+				row.appendChild(input); // Add to DOM
+
+				// Add values from character sheet
+				// Loop through all entries in map
+				rawMap.forEach(entry => {
+					// Check if the current field in the PDF matches an entry in the map
+					if (pdfFieldKey.trim() == entry.pdf) {
+						// Set the input to what is on the character sheet
+						if (field.type === "boolean") { input.checked = entry.foundry } // If it's a checkbox
+						else { input.setAttribute('value', entry.foundry) }; // If it's anything else
+					};
+				});
 			});
 		};
 	};
 
-	fillFields(buffer) {
+	/** Get values and download PDF */
+	download(buffer) {
 		var fieldList = document.getElementById("fieldList");
 		var fields = {};
 		fieldList.querySelectorAll("input, select").forEach((input) => {
@@ -167,9 +200,10 @@ class Pdfconfig extends FormApplication {
 		saveAs(blob, "character-sheet.pdf");
 	};
 
+	/** Manage new PDF upload */
 	onFileUpload(filename, buffer) {
 		currentBuffer = buffer;
-		this.listInputs(currentBuffer);
+		this.createForm(currentBuffer);
 
 		document.getElementById("pdf-upload").setAttribute("style", "display: none");
 		document.getElementById("pdf-download").removeAttribute("disabled");
