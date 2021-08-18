40a11c50f722309559fffd55e80f78b1595ef010
arcanist <arcanistzed@gmail.com>
2021-08-17 22:09:59 -0400

    feat: code cleanup

--- scripts/module.js
+++ scripts/module.js
@@ -40,150 +40,138 @@ class Pdfconfig extends FormApplication {
 	};
 
 	activateListeners(html) {
-		console.log(this)
-
-		this.load()
-
-		document.getElementById("pdf-upload").addEventListener("change", function (e) {
-			var file = e.target.files[0];
+		document.getElementById("pdf-upload").addEventListener("change", event => {
+			var file = event.target.files[0];
 			var reader = new FileReader();
-			reader.onload = function (ev) {
-				onFileUpload(file.name, ev.target.result);
-			};
+			reader.onload = ev => this.onFileUpload(file.name, ev.target.result);
 			reader.readAsArrayBuffer(file);
 		});
 
 		document.getElementById("pdf-download").addEventListener("click", event => {
 			event.preventDefault();
-			fillFields(currentBuffer);
+			this.fillFields(currentBuffer);
 		});
 	};
 
-	load() {
-		const pdflib_radios = document.querySelectorAll("input[name='pdflib']");
-		for (let i = 0;  i < pdflib_radios.length; i++) {
-			let r = pdflib_radios[i];
-			r.addEventListener("change", () => {
-				if (currentBuffer) {
-					listInputs(currentBuffer);
+	listInputs(buffer) {
+		const inputForm = document.getElementById("fieldList");
+
+		// Empty input form
+		let child;
+		while ((child = inputForm.lastChild)) {
+			inputForm.removeChild(child);
+		};
+
+		const pdfFields = pdfform(minipdf).list_fields(buffer);
+
+		const rawMap = [
+			{ "pdf": "CharacterName", "foundry": "@name", "type": "string" },
+			{ "pdf": "Check Box 11", "foundry": "@data.abilities.str.proficient", "type": "string" },
+		];
+
+		const actorData = this.actor.data;
+		console.log(actorData)
+
+		rawMap.forEach(entry => {
+			// Does this entry contain a dynamic key
+			if (entry.foundry.includes("@")) {
+				let paramArray = entry.foundry.slice(1).split(".");
+
+				let current = actorData;
+				paramArray.forEach(param => current += `['${param}']`)
+
+				console.log(current);
+			} else {
+
+			};
+		});
+
+		console.log("pdf fields:", pdfFields);
+
+		for (let pdfFieldKey in pdfFields) {
+
+			let row = document.createElement("li");
+			row.innerText = `${pdfFieldKey}: `;
+			inputForm.appendChild(row);
+
+			pdfFields[pdfFieldKey].forEach((field, i) => {
+				if ((field.type === "radio") && field.options) {
+					let fieldSet = document.createElement("fieldset");
+
+					field.options.forEach((value) => {
+						let radioLabel = document.createElement("label");
+						let radio = document.createElement("input");
+
+						radio.setAttribute("type", "radio");
+						radio.setAttribute("value", value);
+						radio.setAttribute("name", pdfFieldKey + i);
+						radio.setAttribute("data-idx", i);
+						radio.setAttribute("data-key", pdfFieldKey);
+
+						radioLabel.appendChild(radio);
+						radioLabel.innerText = value;
+						fieldSet.appendChild(radioLabel);
+					});
+
+					row.appendChild(fieldSet);
+					return;
 				};
+
+				let input = document.createElement((field.type === "select") ? "select" : "input");
+				input.setAttribute("data-idx", i);
+				input.setAttribute("data-key", pdfFieldKey);
+
+				if (field.type === "boolean") {
+
+					input.setAttribute("type", "checkbox");
+
+				} else if (field.type === "string") {
+
+					//input.setAttribute('value',);
+
+				} else if ((field.type === "select") && field.options) {
+
+					field.options.forEach((value) => {
+
+						let option = document.createElement("option");
+						option.innerText = value;
+						option.setAttribute("value", value);
+						input.appendChild(option);
+					});
+				};
+				row.appendChild(input);
 			});
 		};
 	};
-};
 
-
-// Example of listing all fields
-function listInputs(buffer) {
-	const inputForm = document.getElementById("fieldList");
-	emptyNode(inputForm);
-
-	let count = 1;
-	let pdfFields;
-	try {
-		pdfFields = pdfform(minipdf).list_fields(buffer);
-	} catch (err) {
-		notifyError(err);
-		return;
-	};
-
-	for (let pdfFieldKey in pdfFields) {
-
-		const row = document.createElement("li");
-		row.innerText = `${pdfFieldKey}: `;
-		inputForm.appendChild(row);
-
-		console.log(pdfFields);
-
-		pdfFields[pdfFieldKey].forEach((field, i) => {
-			if ((field.type === "radio") && field.options) {
-				const fieldSet = document.createElement("fieldset");
-
-				field.options.forEach((value) => {
-					const radioLabel = document.createElement("label");
-					const radio = document.createElement("input");
-
-					radio.setAttribute("type", "radio");
-					radio.setAttribute("value", value);
-					radio.setAttribute("name", pdfFieldKey + i);
-					radio.setAttribute("data-idx", i);
-					radio.setAttribute("data-key", pdfFieldKey);
-
-					radioLabel.appendChild(radio);
-					radioLabel.innerText = value;
-					fieldSet.appendChild(radioLabel);
-				});
-
-				row.appendChild(fieldSet);
+	fillFields(buffer) {
+		var fieldList = document.getElementById("fieldList");
+		var fields = {};
+		fieldList.querySelectorAll("input, select").forEach((input) => {
+			if ((input.getAttribute("type") === "radio") && !input.checked) {
 				return;
 			};
 
-			const input = document.createElement((field.type === "select") ? "select" : "input");
-			input.setAttribute("data-idx", i);
-			input.setAttribute("data-key", pdfFieldKey);
-			if (field.type === "boolean") {
-				input.setAttribute("type", "checkbox");
-			} else if (field.type === "string") {
-
-			} else if ((field.type === "select") && field.options) {
-				field.options.forEach((value) => {
-					const option = document.createElement("option");
-					option.innerText = value;
-					option.setAttribute("value", value);
-					input.appendChild(option);
-				});
+			var key = input.getAttribute("data-key");
+			if (!fields[key]) {
+				fields[key] = [];
 			};
-			row.appendChild(input);
+			var index = parseInt(input.getAttribute("data-idx"), 10);
+			var value = (input.getAttribute("type") === "checkbox") ? input.checked : input.value;
+			fields[key][index] = value;
 		});
-	};
-};
 
-// Example of filling out fields
-function fillFields(buffer) {
-	var fieldList = document.getElementById("fieldList");
-	var fields = {};
-	fieldList.querySelectorAll("input, select").forEach((input) => {
-		if ((input.getAttribute("type") === "radio") && !input.checked) {
-			return;
-		};
+		const filled_pdf = pdfform(minipdf).transform(buffer, fields);
 
-		var key = input.getAttribute("data-key");
-		if (!fields[key]) {
-			fields[key] = [];
-		};
-		var index = parseInt(input.getAttribute("data-idx"), 10);
-		var value = (input.getAttribute("type") === "checkbox") ? input.checked : input.value;
-		fields[key][index] = value;
-	});
-
-	var filled_pdf; // Uint8Array
-	try {
-		filled_pdf = pdfform(minipdf).transform(buffer, fields);
-	} catch (e) {
-		return notifyError(e);
+		const blob = new Blob([filled_pdf], { type: "application/pdf" });
+		saveAs(blob, "character-sheet.pdf");
 	};
 
-	var blob = new Blob([filled_pdf], { type: "application/pdf" });
-	saveAs(blob, "character-sheet.pdf");
-};
+	onFileUpload(filename, buffer) {
+		currentBuffer = buffer;
+		this.listInputs(currentBuffer);
 
-
-function notifyError(e) {
-	console.error(e, e.stack);
-	ui.notifications.error(e.message);
-};
-
-function emptyNode(node) {
-	let last;
-	while ((last = node.lastChild)) {
-		node.removeChild(last);
+		document.getElementById("pdf-upload").setAttribute("style", "display: none");
+		document.getElementById("pdf-download").removeAttribute("disabled");
 	};
-};
-
-function onFileUpload(filename, buffer) {
-	currentBuffer = buffer;
-	listInputs(currentBuffer);
-
-	document.getElementById("pdf-upload").setAttribute("style", "display: none");
-	document.getElementById("pdf-download").removeAttribute("disabled");
 };
